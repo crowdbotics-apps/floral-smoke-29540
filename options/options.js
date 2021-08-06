020279abdfac0e8f159cce4df09a979dbd028853
Crowdbotics <team@crowdbotics.com>
2021-08-06 00:29:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "floral_smoke_29540Identifier",
+  url: "https://floral_smoke_29540Identifier.botics.co",
+  api: "https://floral_smoke_29540Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
